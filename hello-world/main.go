a1fdf699fbba50d2af7b76950f378a964dda25cc
breakpointdebug <74284642+breakpointdebug@users.noreply.github.com>
2021-02-02 16:59:07 +0800

    updated code and notes for chapter 3

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("hey")
+	//fmt.Println("hey")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			// _ = throw away returns
+			_, err := fmt.Println(strconv.Itoa(i), 42, true)
+			// fmt.Println(n)
+			fmt.Println(err)
+		}
+	}
 }
