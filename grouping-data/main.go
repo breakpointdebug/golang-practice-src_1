2be31b36b822a72fae3a5f739f3912348a21465b
breakpointdebug <74284642+breakpointdebug@users.noreply.github.com>
2021-02-12 12:48:24 +0800

    slice: append, delete, make, multi-dimensional

--- grouping-data/main.go
+++ grouping-data/main.go
@@ -47,9 +47,76 @@ func fnSlicingASlice() {
 	fmt.Printf("start at index 1, take until 3rd element (x[1:3]):\t%v\n", x[1:3])
 }
 
+func fnAppendValueToSlice() {
+	x := []int{1, 2, 3}
+	// append() is a special built in function just like len()
+	// it is found at "builtin" package
+
+	fmt.Println(append(x, 4, 5, 6)) // append at end of slice
+
+	y := []int{111, 222, 333}
+	fmt.Println(append(x, y...)) // append slice to a slice (should have the same data type)
+}
+
+func fnDeleteFromSlice() {
+	x := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(x)
+	// slice the first 2 elements
+	// slice the elements starting from index 4
+	// then append (combine), ignoring element: 3 & 4
+	fmt.Println(append(x[:2], x[4:]...)) // deleted 3 and 4
+}
+
+func fnSliceMake() {
+	// slices are dynamic, they change size automatically
+
+	// if you know size of the array, use make()
+
+	x := make([]int, 7, 12) // make(<type>, <length, indexes>, <capacity according to it's type>)
+
+	fmt.Printf("slice: \t\t%v\n", x)
+	fmt.Printf("length: \t%v\n", len(x))   // 7
+	fmt.Printf("capacity: \t%v\n", cap(x)) // 12
+
+	x = append(x, 300) // extend length automatically
+	fmt.Printf("slice: \t\t%v\n", x)
+	fmt.Printf("length: \t%v\n", len(x))   // 8
+	fmt.Printf("capacity: \t%v\n", cap(x)) // 12
+
+	x = append(x, 301)
+	x = append(x, 302)
+	x = append(x, 303)
+	x = append(x, 304)
+	x = append(x, 305)
+	fmt.Printf("slice: \t\t%v\n", x)
+	fmt.Printf("length: \t%v\n", len(x))   // 13
+	fmt.Printf("capacity: \t%v\n", cap(x)) // 24
+}
+
+func fnMultiDimensionalSlice() {
+	x := []string{"A", "B", "C"}
+	fmt.Println(x)
+
+	y := []string{"D", "E", "F"}
+	fmt.Println(y)
+
+	z := [][]string{x, y} // multi-dimensional slice
+	fmt.Println(z)
+
+	a := append(x, y...)
+	fmt.Println(a)
+
+	b := append(z, z...)
+	fmt.Println(b)
+}
+
 func main() {
 	// fnArray()
 	// fnSlices()
 	// fnRangeVSSlices()
-	fnSlicingASlice()
+	// fnSlicingASlice()
+	// fnAppendValueToSlice()
+	// fnDeleteFromSlice()
+	// fnSliceMake()
+	fnMultiDimensionalSlice()
 }
